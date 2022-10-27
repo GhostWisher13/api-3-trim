1232fde1603e95772b5fc55577ed08e7bb853d05
José Gabriel Corrêa <101403016+GhostWisher13@users.noreply.github.com>
2022-10-27 14:52:50 -0300

    Add files via upload

---
+++ frontendspotify/spotifyapi/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Body from "../components/BodySpot.vue";
+import Aside from "../components/AsideSpot.vue";
+import Header from "../components/HeaderSpot.vue";
+import Footer from "../components/FooterSpot.vue";
+</script>
+
+<template>
+  <main>
+    <Body />
+    <Aside />
+    <Footer />
+    <Header />
+  </main>
+</template>
